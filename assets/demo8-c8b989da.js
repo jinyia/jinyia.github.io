d84c16628aa6b8d0d0b8a51a5132e2cff68b0bc3
jinyia <122585520+jinyia@users.noreply.github.com>
2023-03-25 10:23:08 +0800

    Add files via upload

---
+++ assets/demo8-c8b989da.js
@@ -0,0 +1,19 @@
+const n=`<template>\r
+  <div class="container">\r
+    <l-input  />\r
+    \r
+  </div>\r
+</template>\r
+\r
+<script setup lang="ts">\r
+\r
+<\/script>\r
+\r
+<style scoped>\r
+.container {\r
+  width: 250px;\r
+}\r
+.item {\r
+  margin-top: 20px;\r
+}\r
+</style>`;export{n as default};
